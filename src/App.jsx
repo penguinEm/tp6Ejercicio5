32612dead3b46bcd8e23e50a81fb463adfc8bd4e
emir paradi <emir.alejandro.paradi@gmail.com>
2024-01-24 18:44:11 -0300

    Hicimos el componente Navbar y le agregamos estilos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Footer from "./components/Footer";
 import FormularioTareas from "./components/FormularioTareas";
 import { Container } from "react-bootstrap";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar></Navbar>
       <Container className="my-5">
         <h1 className="text-center display-3 color-titulo my-5">
           Lista de Tareas
